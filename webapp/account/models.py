6947986deea3f1fe7140a08a53f62604801555dd
dimmy2000 <dimsamoylenko@gmail.com>
2021-04-13 00:15:57 +0300

    добавил модель таблицы хэшей

--- webapp/account/models.py
+++ webapp/account/models.py
@@ -20,7 +20,25 @@ class Account(db.Model):
     currency = db.Column(db.String(3))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     items = relationship("Item", lazy="joined")
+    hashes = relationship("Hash", lazy="joined")
 
     def __repr__(self):
         """Определяем формат вывода объекта класса Account."""
         return f"Account {self.username}."
+
+
+class Hash(db.Model):
+    """Модель таблицы хэшей данных об аккаунтах Steam."""
+
+    __tablename__ = "hashes"
+
+    hash_id = db.Column(db.Integer, primary_key=True)
+    account_hash = db.Column(db.BINARY(128))
+    account_last_updated = db.Column(db.DateTime)
+    inventory_hash = db.Column(db.BINARY(128))
+    inventory_last_updated = db.Column(db.DateTime)
+    account_id = db.Column(db.Integer, db.ForeignKey('accounts.account_id'))
+
+    def __repr__(self):
+        """Определяем формат вывода объекта класса Hash."""
+        return f"Hash id: {self.hash_id}."
